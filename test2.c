0fd00b2b24b8b909ed51d6ab3c04a27731b28551
gcf666666 <58467375+gcf666666@users.noreply.github.com>
2019-12-03 19:59:52 +0800

    加-

--- test2.c
+++ test2.c
@@ -4,7 +4,7 @@ int main(void)/*dadadadada*/
     int a = 1, b = 1;
     int aplup,plusb;
 
-    aplus = a++;
+    aplus = a++-;
     plusb = ++b;
     printf("a   aplus  b  plusb \n");
     printf("%1d %5d %5d %5d\n", a, aplus, b, plusb);
